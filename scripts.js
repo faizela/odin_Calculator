2261604afe8eb44ea3bbc5f6bfdd77d5b59e312a
feisal ahmed <feisala132@gmail.com>
2022-06-21 18:06:57 +0100

    Adding EventListeners to cols

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+const display = document.getElementById('display');
+const col_1_btns = document.getElementsByClassName("col_1-btns");
+const col_2_btns = document.getElementsByClassName("col_2-btns");
+const col_3_btns = document.getElementsByClassName("col_3-btns");
+
+let currentOperation = ''
+
+display_val = function (btn) {
+  display.innerText += btn.innerText
+  currentOperation = display.textContent
+}
+
+
 function add (a, b) {
   return a + b
 }
@@ -19,7 +32,7 @@ function operate (operator, a, b) {
   operations = {
     "+" : (a,b) => a + b, 
     "-" : (a,b) => a - b,
-    "*" : (a,b) => a * b,
+    "*" : (a,b) => a * b, 
     "/" : (a,b) => a / b,
 
 }
@@ -27,6 +40,29 @@ function operate (operator, a, b) {
 return operations[operator] (a,b)
 
 }
+
+function col_1_add_EV() {
+  for (let btn of col_1_btns){
+  btn.addEventListener('click', () =>  display_val(btn))
+  }
+}
+
+function col_2_add_EV () {
+  for (let btn of col_2_btns){
+    btn.addEventListener('click',() => display_val(btn))
+  }
+}
+
+function col_3_add_EV() {
+  for (let btn of col_3_btns){
+    btn.addEventListener('click',() => display_val(btn))
+  }
+}
+
+
+col_1_add_EV()
+col_2_add_EV()
+col_3_add_EV()
 
 console.log(add(2,3))
 console.log(substract(3,2))
